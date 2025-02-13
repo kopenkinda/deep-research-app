49d5a5637fa2dd8123a66026c1989afe058f1532
Kopenkin Dmitrii <dmitrii@moveworkforward.com>
2025-02-13 01:37:23 +0100

    feat: add async iterable output validation

--- apps/server/src/router.ts
+++ apps/server/src/router.ts
@@ -1,7 +1,9 @@
 import { initTRPC } from "@trpc/server";
 import SuperJSON from "superjson";
+import { z } from "zod";
 import type { Context } from "./context";
 import { updateSchema } from "./schemas";
+import { zAsyncIterable } from "./utils/zod-async-iterable";
 
 const transformer = new SuperJSON();
 const t = initTRPC.context<Context>().create({
@@ -20,15 +22,17 @@ export const appRouter = router({
       status: "active",
     };
   }),
-  stream: publicProcedure.subscription(async function* handler({ ctx }) {
-    for (let i = 0; i < 100; i++) {
-      await new Promise((resolve) => setTimeout(resolve, 500));
-      if (i > 10) {
-        return;
+  stream: publicProcedure
+    .output(zAsyncIterable({ yield: z.number() }))
+    .subscription(async function* handler({ ctx }) {
+      for (let i = 0; i < 100; i++) {
+        await new Promise((resolve) => setTimeout(resolve, 500));
+        if (i > 10) {
+          return;
+        }
+        yield i;
       }
-      yield i;
-    }
-  }),
+    }),
 });
 
 export const createCaller = createCallerFactory(appRouter);
